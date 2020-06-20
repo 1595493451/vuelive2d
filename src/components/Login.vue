593ad345659d69b09d40b6da1f26e57f80bbc368
1595493451 <1595493451@qq.com>
2020-06-21 02:27:54 +0800

    333

---
+++ src/components/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="login_container"></div>
+</template>
+
+<script>
+export default {}
+</script>
+<script src="../../live2d-widget/autoload.js"></script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+}
+</style>
